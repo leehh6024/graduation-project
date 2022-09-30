1b16948f4edd0193d62e3240fd2ac498689cdda8
leehh6024 <dlgusghchlrh@gmail.com>
2022-09-30 22:34:38 +0900

    change bottom-sheet size

--- src/pages/main/mainPage.js
+++ src/pages/main/mainPage.js
@@ -5,32 +5,32 @@ import { useContext, useEffect } from "react";
 import "./mainPage.css";
 
 export default function Main() {
-	const { state, setState } = useContext(GlobalContext);
+  const { state, setState } = useContext(GlobalContext);
 
-	useEffect(() => {
-		setState((prev) => ({ ...prev, sheet: false }));
-	}, []);
+  useEffect(() => {
+    setState((prev) => ({ ...prev, sheet: false }));
+  }, []);
 
-	function onDismiss() {
-		setState((prev) => ({ ...prev, sheet: false }));
-	}
-	return (
-		<>
-			{/* <button onClick={() => setState((prev) => ({ ...prev, sheet: !state.sheet }))}>
+  function onDismiss() {
+    setState((prev) => ({ ...prev, sheet: false }));
+  }
+  return (
+    <>
+      {/* <button onClick={() => setState((prev) => ({ ...prev, sheet: !state.sheet }))}>
 				{state.sheet ? "close" : "open"}
 			</button> */}
-			<Map />
-			<BottomSheet
-				open={state.sheet}
-				onDismiss={onDismiss}
-				snapPoints={({ maxHeight }) => [0.45 * maxHeight, 0.83 * maxHeight]}
-				defaultSnap={({ lastSnap, snapPoints }) => {
-					lastSnap ?? Math.max(...snapPoints);
-				}}
-				blocking={false}
-			>
-				<input autoComplete="off" className="commentInput" type="text" />
-			</BottomSheet>
-		</>
-	);
+      <Map />
+      <BottomSheet
+        open={state.sheet}
+        onDismiss={onDismiss}
+        snapPoints={({ maxHeight }) => [0.45 * maxHeight, 0.83 * maxHeight]}
+        defaultSnap={({ lastSnap, snapPoints }) => {
+          lastSnap ?? Math.max(...snapPoints);
+        }}
+        blocking={false}
+      >
+        <input autoComplete="off" className="commentInput" type="text" />
+      </BottomSheet>
+    </>
+  );
 }
