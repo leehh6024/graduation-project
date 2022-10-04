5cd29db3387f3ebd0c28a97976f446581ec60d9c
leehh6024 <dlgusghchlrh@gmail.com>
2022-10-04 14:14:42 +0900

    ...

--- src/pages/main/components/Map.jsx
+++ src/pages/main/components/Map.jsx
@@ -2,9 +2,51 @@ import React, { useState, useEffect, useCallback, useContext } from "react";
 import GlobalContext from "../../../common/context/store";
 import "./Map.css";
 import { API } from "../../../service.js";
+import axios from "axios";
 
 const { kakao } = window;
 var container, options, map;
+
+function getInfo() {
+	// var center = map.getCenter();
+	// var level = map.getLevel();
+	// var mapTypeId = map.getMapTypeId();
+	var bounds = map.getBounds();
+	var swLatLng = bounds.getSouthWest();
+	var neLatLng = bounds.getNorthEast();
+	console.log(swLatLng);
+	console.log(neLatLng);
+
+	const User = {
+		southWest: {
+			lat: swLatLng.La,
+			lng: swLatLng.Ma,
+		},
+		northEast: {
+			lat: neLatLng.La,
+			lng: neLatLng.Ma,
+		},
+	};
+	console.log(User);
+}
+
+const processedIssuePoint = (data) => {
+	const { fixedIssuePointList } = data.data;
+	// console.log(fixedIssuePointList);
+	const processedLocation = fixedIssuePointList.map((issuePoint) => {
+		return {
+			title: issuePoint.title,
+			class: issuePoint.class,
+			latlng: new kakao.maps.LatLng(
+				Number(issuePoint.location.lat),
+				Number(issuePoint.location.lng)
+			),
+			body: issuePoint.body,
+			img: issuePoint.img,
+		};
+	});
+	return processedLocation;
+};
 
 export default function Location() {
 	const { state, setState } = useContext(GlobalContext);
@@ -13,25 +55,32 @@ export default function Location() {
 
 	// 서버와의 통신
 	const getIssuePoints = useCallback(async () => {
-		const { data } = await API.getFixedPointIssue();
+		const { data } = await API.getUserPointIssues(User);
 		// console.log(data);
+
 		if (data) {
-			const { fixedIssuePointList } = data.data;
-			// console.log(fixedIssuePointList);
-			const processedLocation = fixedIssuePointList.map((issuePoint) => {
+			const processedLocation = processedIssuePoint(data);
+			setLocations(processedLocation);
+		}
+
+		const { data } = await API.getUserPointIssues();
+		if (data) {
+			const { userIssuePointList } = data.data;
+			const processedBound = userIssuePointList.map((boundPoint) => {
 				return {
-					title: issuePoint.title,
-					class: issuePoint.class,
-					latlng: new kakao.maps.LatLng(
-						Number(issuePoint.location.lat),
-						Number(issuePoint.location.lng)
-					),
-					body: issuePoint.body,
-					img: issuePoint.img,
+					User: {
+						southWest: {
+							lat: Number(boundPoint.body.user.southWest.lat),
+							lng: Number(boundPoint.body.user.southWest.lng),
+						},
+						northEast: {
+							lat: Number(boundPoint.body.user.northEast.lat),
+							lng: Number(boundPoint.body.user.northEast.lng),
+						},
+					},
 				};
 			});
-			console.log(processedLocation);
-			setLocations(processedLocation);
+			// console.log(processedBound);
 		}
 	}, []);
 
@@ -44,6 +93,43 @@ export default function Location() {
 		};
 		map = new kakao.maps.Map(container, options);
 	}, []);
+
+	// user currentlocation
+
+	const currentLocation = () => {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function (position) {
+				var lat = position.coords.latitude,
+					lng = position.coords.longitude;
+
+				var locPosition = new kakao.maps.LatLng(lat, lng);
+
+				displayMarker(locPosition);
+				getInfo();
+			});
+		} else {
+			var locPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+
+			displayMarker(locPosition);
+		}
+	};
+
+	function displayMarker(locPosition) {
+		var marker = new kakao.maps.Marker({
+			map: map,
+			position: locPosition,
+		});
+
+		var iwRemoveable = true;
+
+		var infoWindow = new kakao.maps.InfoWindow({
+			removable: iwRemoveable,
+		});
+
+		infoWindow.open(map, marker);
+
+		map.setCenter(locPosition);
+	}
 	/**
 	 * getIssuePoints 는 useCallback() 을 통해 생성된 함수이므로
 	 * state 가 변경되어도 재선언되지 않음
@@ -51,6 +137,7 @@ export default function Location() {
 	 */
 	useEffect(() => {
 		getIssuePoints();
+		currentLocation();
 	}, [getIssuePoints]);
 	/**
 	 * locations 는 state 변수이므로, 값이 변경될 때(서버에서 데이터를 받아올 때)
